523fa6f782d3a417d605cad179050f0b58a3d26b
nerdfirefighter <29542626+nerdfirefighter@users.noreply.github.com>
2018-03-13 08:03:54 +1100

    Added Unit Declaration
    Added the declaring of "unit" in the constructor.
    Added setUnit, can only be called before the onReadyToOperate. There may be times when you want to change it after the sensor is ready to operator. But for now, once node is ready, its set. 
    Have not tested the code. 
    Good work Tim

--- src/SensorNode.hpp
+++ src/SensorNode.hpp
@@ -8,22 +8,35 @@ const char UNIT[] = "unit";
 class SensorNode : HomieNode
 {
 public:
-  SensorNode(const char* id);
+  SensorNode(const char* id, const char* unit);
   void setValue(const String& value);
+  bool setUnit(const char* unit);
 private:
   void onReadyToOperate();
+  char* node_unit;
+  bool unitSent;
 };
 
-SensorNode::SensorNode(const char* id): HomieNode(id, "sensor")
+SensorNode::SensorNode(const char* id, const char* unit): HomieNode(id, "sensor")
 {
+  node_unit = unit;
   advertise(VALUE);
   advertise(UNIT);
+  unitSent = false;
 }
 
 void SensorNode::onReadyToOperate(){
-  setProperty(UNIT).send("unit");
+  setProperty(UNIT).send(node_unit);
+  unitSent = true;
 }
 
 void SensorNode::setValue(const String& value){
   setProperty(VALUE).send(value);
+}
+
+
+void SensorNode::setUnit(const char* unit){
+  if (unitSent) return false;
+  node_unit = unit;
+  return true;
 }
